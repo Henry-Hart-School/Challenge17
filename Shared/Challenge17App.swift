f07a3f635c4c6cdf5afddc683df64d07609ec31e
Henry-Hart-School <hart.h@etoncollege.org.uk>
2022-11-18 16:55:06 +0000

    Initial Commit

---
+++ Shared/Challenge17App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge17App.swift
+//  Shared
+//
+//  Created by Hart, Henry (AGDF) on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Challenge17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
